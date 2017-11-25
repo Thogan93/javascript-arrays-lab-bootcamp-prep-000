080772b98cd6a4a0bdc3fff83c60f3af15603905
Timothy Hogan <tim.hogan93@gmail.com>
2017-11-25 18:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  var kittens = kittens.push();
+  console.log(kittens);
+  return name;
+}
+destructivelyAppendKitten('Ralph');
